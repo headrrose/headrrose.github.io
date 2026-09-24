363589952b47a9034365a08f971faefd434a6a24
Heather Rose <headr.rose@gmail.com>
2026-09-25 10:25:30 +1200

    I forgot what I changed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
-const music = document.getElementById('background-music');
-document.addEventListener('click', () =>{
-    music.play();
-}, {once: true});
+const popupOverlay = document.getElementById('exitPopupOverlay');
+const closePopupBtn = document.getElementById('closePopupBtn');
+let popupShown = false;
 
-function checkQuestion(answer){
-    if (answer == 'correct'){
-        document.getElementById('answer1').innerHTML = "You're correct!"
-    } else if (answer == 'incorrect'){
-        document.getElementById('answer1').innerHTML = "Wrong :P"
-    }
-}
+// Trigger popup on exit intent
+document.addEventListener('mouseleave', (e) => {
+  // e.clientY < 0 checks if the cursor has moved above the top edge of the window
+  if (e.clientY < 0 && !popupShown) {
+    popupOverlay.classList.add('show');
+    popupShown = true; // Prevents the popup from firing repeatedly
+  }
+});
+
+// Close popup when clicking the button
+closePopupBtn.addEventListener('click', () => {
+  popupOverlay.classList.remove('show');
+});
+
+// Close popup when clicking outside the box (on the overlay)
+popupOverlay.addEventListener('click', (e) => {
+  if (e.target === popupOverlay) {
+    popupOverlay.classList.remove('show');
+  }
+});
